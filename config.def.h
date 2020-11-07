bf13f883d97e9e7e2ac17d658e8b9e4a30a25e17
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2020-11-07 23:41:52 +0100

    Add padding to status bar

--- config.def.h
+++ config.def.h
@@ -15,6 +15,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 10;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 10;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=12" };
 static char dmenufont[]             = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
